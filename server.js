1a34eaebffb4a468d0a927ef878c58d76b158970
izcheung <icheung24@my.bcit.ca>
2025-10-03 17:11:29 -0700

    fix: typo

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const PORT = 3000;
 app.use(express.static(path.join(__dirname)));
 
 app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
+  console.log(`Server is listening on port ${PORT}`);
 });
 
 console.log(__dirname);
